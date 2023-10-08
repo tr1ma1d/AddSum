21aa53f0a14fedbb6215b5e60dbbaa2a38db68bd
Nikita <Nikita@DESKTOP-UQUN9E3>
2023-10-08 22:04:35 +0300

    Добавьте файлы проекта.

---
+++ leetCode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            Dictionary<int,int> map = new Dictionary<int,int>();
+            for(int i = 0; i < nums.Length; i++)
+            {
+                int containers = target - nums[i];
+                if(map.ContainsKey(containers))
+                {
+                    Console.WriteLine($"{ map[containers]}, { i} ");
+                }
+                map[nums[i]] = i;
+            }
+            Console.WriteLine();
+        }
+    }
+}
